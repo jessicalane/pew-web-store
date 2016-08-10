9350981ee1ef0631881e736ed48316284d5d6e66
Jessica Lane <jlane@pewabic.org>
2016-08-10 15:44:06 -0400

    attempting to hide nav elements on mobile

--- js/app.js
+++ js/app.js
@@ -3,13 +3,17 @@ $(document).ready(function() {
 // Mobile navigation functions for scrolling/clicking
 	function mobileNav() {
 
-			$(document).on('click', function() {
-				$('.collapse').collapse('hide');
-			});
+		$(document).on('click', function() {
+			$('.collapse').collapse('hide');
+		});
 
-			$(document).on('scroll', function() {
-				$('.collapse').collapse('hide');
-			});
-		}
+		$(document).on('scroll', function() {
+			$('.collapse').collapse('hide');
+		});
+	};
+
+	if ($(window).width < 1000) {
+			console.log("yay!")
+	}
 
 });
