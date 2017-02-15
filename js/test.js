53291b99fcd40e3b961c6b774d79f5eada5cdfb7
Jessica Lane <jessica.lynn.brasher@gmail.com>
2017-02-15 11:57:30 -0500

    attempting solution for out of stock child items

---
+++ js/test.js
@@ -0,0 +1,18 @@
+function loginStyle() {
+		$('a:contains("Register")').addClass("register-link");
+		$('.smalltext:contains("Log")').addClass("login-link");
+	}
+
+	$( "input[name^='news']" ).val( "news here!" );
+
+<script type="text/javascript">
+	$(document).ready(function() {
+		$('#custcolglazes').change(function(){
+			if($("span[id^='itemavail']").prototype.contains("Out of Stock")) {
+				console.log("outofstock");
+			}else {
+				console.log("instock");
+			}
+		});
+	});
+</script>
